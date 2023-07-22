92f70a55fc2b29f568be1955ae24f64af06ad798
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-22 16:47:43 +0900

    Feat : description 추가

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'haseung',
   headerTitle: "하승's blog",
-  description: '',
+  description: 'Tech blog',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
