4877bed37fdbcbfb6ea0a0269c4b7c0ab5d8d7c5
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-07-22 14:06:52 +0900

    Delete : description 내용 삭제

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'haseung',
   headerTitle: "하승's blog",
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
