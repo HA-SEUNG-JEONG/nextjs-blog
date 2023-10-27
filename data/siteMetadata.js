912bb10f5fe4063ad80415072bc284ef7a4ee7f7
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-10-27 23:45:41 +0900

    Feat : siteMetadata 수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -40,8 +40,8 @@ const siteMetadata = {
       // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      category: 'Comment',
+      categoryId: 'DIC_kwDOJ-LwF84Cafmb',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
